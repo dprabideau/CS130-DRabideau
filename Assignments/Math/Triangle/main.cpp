771dcec4b56c8b4523f534845be29ae2e9f049e6
dprabideau <dpatrickrabideau@gmail.com>
2024-08-26 16:35:23 -0600

    1

---
+++ Assignments/Math/Triangle/main.cpp
@@ -0,0 +1,8 @@
+/*=================================================
+  Triangle Math program
+  Author: Dayne P Rabideau
+  Date: Aug-26-2024
+  
+  The program allows the user to input values to 
+  calculate properties of a triangle.
+===================================================*/
