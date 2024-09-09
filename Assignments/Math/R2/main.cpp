b81baad3a401a0eff5e77c9f0498eebb75a4e1ed
dprabideau <127996630+dprabideau@users.noreply.github.com>
2024-09-09 23:07:04 +0000

    HW2.1Done-Need Makefile test rule

---
+++ Assignments/Math/R2/main.cpp
@@ -0,0 +1,20 @@
+/*=================================================
+  Triangle Math program
+  Author: Dayne P Rabideau
+  Date: Aug-26-2024
+  
+  The program finds the forgotten interger from the
+  mean value of two integers
+===================================================*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int R1, R2, S1;
+    cin >> R1;
+    cin >> S1;
+    R2 = (2*S1)-R1;
+    cout << R2;
+}
